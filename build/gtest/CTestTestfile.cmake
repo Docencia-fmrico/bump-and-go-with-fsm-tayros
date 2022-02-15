40973b3d41cc599da965f52c69c885fc3448e45e
Ivan-Porras <ivanporras44@gmail.com>
2022-02-15 18:16:21 +0100

    build remove

--- build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest
-# Build directory: /home/pocho/Git/Segundo_Curso-/arq_ros/ros1/catkin_ws/src/bump-and-go-with-fsm-tayros/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- build/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/pocho/Git/Segundo_Curso-/arq_ros/ros1/catkin_ws/src/bump-and-go-with-fsm-tayros/build/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
